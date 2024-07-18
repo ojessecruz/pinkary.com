961452573fe86b05d13f06da555a2f9e68819eff
Alexander Wietz <alexander@wietz.dk>
2024-07-18 21:11:51 +0200

    Add support for twitter x.com domain

--- app/Livewire/Home/Search.php
+++ app/Livewire/Home/Search.php
@@ -114,7 +114,8 @@ final class Search extends Component
         $famousUsers = Cache::remember('top-50-users', now()->endOfDay(), fn (): array => User::query()
             ->whereHas('links', function (Builder $query): void {
                 $query->where('url', 'like', '%twitter.com%')
-                    ->orWhere('url', 'like', '%github.com%');
+                    ->orWhere('url', 'like', '%github.com%')
+                    ->orWhere('url', 'like', '%://x.com%');
             })
             ->whereNotIn('id', $except->pluck('id'))
             ->withCount(['questionsReceived as answered_questions_count' => function (Builder $query): void {
@@ -141,7 +142,8 @@ final class Search extends Component
         return User::query()
             ->whereHas('links', function (Builder $query): void {
                 $query->where('url', 'like', '%twitter.com%')
-                    ->orWhere('url', 'like', '%github.com%');
+                    ->orWhere('url', 'like', '%github.com%')
+                    ->orWhere('url', 'like', '%://x.com%');
             })
             ->where(function (Builder $query): void {
                 $query->where('is_verified', true)
