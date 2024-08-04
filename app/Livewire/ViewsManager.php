67e3ade7883d70f6e9fa963c523b507462182e51
Punyapal Shah <mrpunyapal@gmail.com>
2024-08-04 21:46:23 +0530

    feat: Add ViewsManager component for managing views

---
+++ app/Livewire/ViewsManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ViewsManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.views-manager');
+    }
+}
