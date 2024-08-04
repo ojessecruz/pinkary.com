88893f99ba6574f25ba546cf713d6722a3bc2f0f
Punyapal Shah <mrpunyapal@gmail.com>
2024-08-04 21:51:20 +0530

    refactor: Update ViewsManager component to track and send viewed posts
    The ViewsManager component in the livewire/views-manager.blade.php file has been updated to include functionality for tracking and sending viewed posts. This is achieved by adding a new x-data object with properties and methods for managing the viewedPosts array, adding viewed posts to the array, and sending the viewed posts to the server using the $wire.call method.
    
    The component also includes an init method that sets up an interval to periodically call the sendViewedPosts method every 5 seconds. Additionally, the method is called whenever a livewire:navigate event is triggered.
    
    This commit addresses the need to track and send viewed posts in the ViewsManager component, improving the overall functionality of the application.

--- resources/views/livewire/views-manager.blade.php
+++ resources/views/livewire/views-manager.blade.php
@@ -1,3 +1,23 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div x-data="{
+    viewedPosts: [],
+    addViewedPost(postId) {
+        this.viewedPosts = [...this.viewedPosts, postId];
+    },
+    sendViewedPosts() {
+        if (this.viewedPosts.length > 0) {
+            this.$wire.call('updateViews', this.viewedPosts);
+            this.viewedPosts = [];
+        }
+    },
+    init() {
+        setInterval(() => {
+            this.sendViewedPosts();
+        }, 5000);
+
+        document.addEventListener('livewire:navigate', () => {
+            this.sendViewedPosts();
+        });
+    }
+}" x-on:post-viewed.window="addViewedPost($event.detail.postId)"
+>
 </div>
