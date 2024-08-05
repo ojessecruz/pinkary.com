f9851e81c5477742557bbf3696db9feeebafb615
Punyapal Shah <mrpunyapal@gmail.com>
2024-08-05 21:28:03 +0530

    refactor: Update intersection observer event in show.blade.php

--- resources/views/livewire/questions/show.blade.php
+++ resources/views/livewire/questions/show.blade.php
@@ -32,7 +32,7 @@
     @if ($question->answer)
         <div
             data-parent=true
-            x-intersect.once.half="$dispatch('post-viewed', { postId: '{{ $questionId }}' })"
+            x-intersect.once.full="$dispatch('post-viewed', { postId: '{{ $questionId }}' })"
             x-data="clickHandler"
             x-on:click="handleNavigation($event)"
             class="group p-4 mt-3 rounded-2xl {{ $previousQuestionId === $questionId ? 'bg-slate-700/60' : 'bg-slate-900' }}
